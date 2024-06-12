45db2fd2384d754f2eeee80fb1023bbee0ac63a4
afonsoDavis <vissionplayer@gmail.com>
2024-06-12 10:26:26 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(800, 800);
+  }
+  
+  function draw() {
+    background(0);
+    stroke(288)
+  fill(78)
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 28, 35)
+    }
+    
+  }
